57f0aa517925a0e170ca8422cf669944c1f499c7
joaoVicCampos <ripjv1234@gmail.com>
2026-05-21 11:18:32 -0300

    fix: ajusta ordem para decrescente(exercicio 11)

--- src/e11.ts
+++ src/e11.ts
@@ -3,7 +3,7 @@ import { arr } from "./util.ts";
 const pessoas =  arr.map((pessoa) => pessoa)
 const ordenadas = pessoas.sort((a, b) => {
     if(a.idade !== b.idade){
-        return a.idade - b.idade
+        return b.idade - a.idade
     }
     return a.id - b.id
 });
