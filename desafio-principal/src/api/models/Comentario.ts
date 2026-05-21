86ee76cde85558947f53315e653b08c0e249c767
joaoVicCampos <ripjv1234@gmail.com>
2026-05-21 17:05:44 -0300

    feat: Cria o model de comentário

---
+++ desafio-principal/src/api/models/Comentario.ts
@@ -0,0 +1,21 @@
+import sequelize from "../database/connection.js";
+import { DataTypes } from "sequelize";
+import Post from "./Post.js";
+
+
+const Comentario = sequelize.define('Comentario', {
+    conteudo: {
+        type: DataTypes.TEXT,
+        allowNull: false
+    },
+    postId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: Post,
+            key: 'id'
+        }
+    }
+});
+
+export default Comentario;
