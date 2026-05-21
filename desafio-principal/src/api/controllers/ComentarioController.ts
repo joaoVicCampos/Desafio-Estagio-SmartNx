5a71592181f5619df8a92def0026a1292e33d072
joaoVicCampos <ripjv1234@gmail.com>
2026-05-21 17:23:27 -0300

    feat: implementa rota POST de comentários

---
+++ desafio-principal/src/api/controllers/ComentarioController.ts
@@ -0,0 +1,25 @@
+import type { Request, Response } from 'express';
+import Post from '../models/Post.js';
+import Comentario from '../models/Comentario.js';
+
+class ComentarioController {
+    async create(req: Request, res: Response){
+        try {
+            const postId = Number(req.params.postId);
+            const post = await Post.findByPk(postId)
+            const {conteudo} = req.body;
+            if (!conteudo) {
+                return  res.status(400).json({message: 'Comentário sem conteúdo'})
+            }
+            const comentario = await Comentario.create({
+                conteudo,
+                postId
+            })
+            return res.status(201).json(comentario)
+        } catch (error) {
+            return res.status(500).json({message: 'Erro ao adicionar comentário'})
+        }
+    }
+}
+
+export default new ComentarioController();
--- desafio-principal/src/api/routes.ts
+++ desafio-principal/src/api/routes.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import PostController from "./controllers/PostController.js";
+import ComentarioController from "./controllers/ComentarioController.js";
 
 const routes = Router();
 
 routes.post('/posts', PostController.create);
 routes.get('/posts', PostController.findAll);
 routes.delete('/posts/:id', PostController.delete);
+routes.post('/posts/:postId/comentarios', ComentarioController.create)
 
 
 export default routes;
