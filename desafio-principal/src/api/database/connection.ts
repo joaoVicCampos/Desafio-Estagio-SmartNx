56caedfc33f3c799f1123f1fe23b20e50bece627
joaoVicCampos <ripjv1234@gmail.com>
2026-05-21 13:40:26 -0300

    fix: migra connection para Sequelize

--- desafio-principal/src/api/database/connection.ts
+++ desafio-principal/src/api/database/connection.ts
@@ -1,5 +1,6 @@
 import mysql from 'mysql2/promise';
 import 'dotenv/config';
+import { Sequelize } from 'sequelize';
 
 const {
   DB_HOST,
@@ -12,11 +13,9 @@ if (!DB_HOST || !DB_USER || !DB_PASSWORD || !DB_NAME) {
   throw new Error('Variáveis de ambiente do banco não configuradas.');
 }
 
-const pool = mysql.createPool({
+const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD , {
   host: DB_HOST,
-  user: DB_USER,
-  password: DB_PASSWORD,
-  database: DB_NAME,
-});
+  dialect: 'mysql'
+})
 
-export default pool;
+export default sequelize;
